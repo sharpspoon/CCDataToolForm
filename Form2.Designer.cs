4e0b9447315c5cc0af599202f7296878fda4afef
robin ward <robin@steelcitysites.net>
2018-02-08 17:10:54 -0600

    cleanup

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -35,12 +35,13 @@
             this.button8 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(199, 70);
+            this.textBox7.Location = new System.Drawing.Point(196, 72);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(83, 26);
             this.textBox7.TabIndex = 32;
@@ -49,7 +50,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(195, 47);
+            this.label8.Location = new System.Drawing.Point(192, 49);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(87, 20);
             this.label8.TabIndex = 31;
@@ -59,7 +60,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(86, 70);
+            this.label2.Location = new System.Drawing.Point(86, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 20);
             this.label2.TabIndex = 34;
@@ -68,7 +69,7 @@
             // button8
             // 
             this.button8.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button8.Location = new System.Drawing.Point(199, 122);
+            this.button8.Location = new System.Drawing.Point(199, 206);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(83, 40);
             this.button8.TabIndex = 36;
@@ -90,17 +91,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 99);
+            this.label1.Location = new System.Drawing.Point(33, 143);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(360, 20);
+            this.label1.Size = new System.Drawing.Size(427, 60);
             this.label1.TabIndex = 38;
-            this.label1.Text = "You will soon be prompted to enter your password";
+            this.label1.Text = "Upon clicking login, the application will CLOSE and you will\r\nbe prompted to ente" +
+    "r your password. Please save any work \r\nbefore proceeding.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(195, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "WARNING!";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(490, 189);
+            this.ClientSize = new System.Drawing.Size(490, 281);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button8);
@@ -124,5 +137,6 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
     }
 }
