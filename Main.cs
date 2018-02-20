24f160eeade2fbf40bca43a131c06cc716bf647d
robin ward <robin@steelcitysites.net>
2018-02-20 13:56:22 -0600

    date format work

--- Main.cs
+++ Main.cs
@@ -40,20 +40,31 @@ namespace CCDataImportTool
         //------------------DATE CONVERTER START------------------------------------------------------
         private void dateConvert_Click1(object sender, EventArgs e)
         {
-            try
+            if (textBox2.Text.Length == 0)
             {
-                string newPattern = "yyyyMMdd";
-                DateTime thisDate1 = new DateTime();
-                dataGridView1.Columns[textBox2.Text].DefaultCellStyle.Format = thisDate1.ToString(newPattern);
+                MessageBox.Show("You did not enter a column name!\r\nThe operation will now cancel.", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
             }
-            catch (Exception ex)
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
-                if (textBox2.Text.Length == 0)
+                try
                 {
-                    MessageBox.Show("You did not enter a column name!\r\nThe operation will now cancel.", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    var value = dataGridView1.Rows[i].Cells[textBox2.Text].Value.ToString();
+                    int year = int.Parse(value.Substring(0, 4));
+                    int month = int.Parse(value.Substring(4, 2));
+                    int day = int.Parse(value.Substring(6, 2));
+                    if (year > 2200)
+                    {
+                        MessageBox.Show("Error at line " + (i + 1)+"\r\n"+"The year is " +year+", which is greater than 2200.\r\nMake sure that the date is in the format: yyyyMMdd", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Dates are OK");
                     return;
                 }
-                MessageBox.Show(ex.Message, "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         private void textBox2_TextChanged(object sender, EventArgs e)
