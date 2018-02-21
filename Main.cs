b506303e6f068a253033dda18ed3806f8a34d3bb
robin ward <robin@steelcitysites.net>
2018-02-21 10:24:28 -0600

    a

--- Main.cs
+++ Main.cs
@@ -40,20 +40,31 @@ namespace CCDataImportTool
         //------------------DATE CONVERTER START------------------------------------------------------
         private void dateConvert_Click1(object sender, EventArgs e)
         {
+            
             if (textBox2.Text.Length == 0)
             {
                 MessageBox.Show("You did not enter a column name!\r\nThe operation will now cancel.", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
                 return;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                var value = dataGridView1.Rows[i].Cells[textBox2.Text].Value.ToString();
+                if ((value.Length != 8) && (value != null) && (value !=""))
+                    {
+                    MessageBox.Show("Error at line " + (i + 1) + "\r\n" + "Make sure that the date is in the format: yyyyMMdd", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    return;
+                }
             }
 
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
                 try
                 {
-                    var value = dataGridView1.Rows[i].Cells[textBox2.Text].Value.ToString();
-                    int year = int.Parse(value.Substring(0, 4));
-                    int month = int.Parse(value.Substring(4, 2));
-                    int day = int.Parse(value.Substring(6, 2));
+                    var value2 = dataGridView1.Rows[i].Cells[textBox2.Text].Value.ToString();
+                    int year = int.Parse(value2.Substring(0, 4));
+                    int month = int.Parse(value2.Substring(4, 2));
+                    int day = int.Parse(value2.Substring(6, 2));
+
                     if (year > 2200)
                     {
                         MessageBox.Show("Error at line " + (i + 1)+"\r\n"+"The year is " +year+", which is greater than 2200.\r\nMake sure that the date is in the format: yyyyMMdd", "CCDataTool", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
